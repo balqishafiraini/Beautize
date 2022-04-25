3f7d4593fffa7cb111f1846209af8cd9efceea4c
Balqis Shafira Aini <balqisshafiraaini@gmail.com>
2022-04-25 23:38:47 +0700

    Initial Commit

---
+++ Beautize/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Beautize
+//
+//  Created by Balqis on 25/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
